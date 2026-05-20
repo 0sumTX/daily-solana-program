a17ff1c3255b91e62f27c11aff23c8e0ce8c7b78
0sumTX <0sumtx@gmail.com>
2026-05-19 23:20:57 -0700

    fix: update counter

--- days/counter-2026-05-19/programs/counter-2026-05-19/src/lib.rs
+++ days/counter-2026-05-19/programs/counter-2026-05-19/src/lib.rs
@@ -6,10 +6,16 @@ declare_id!("EXV3KYGvRm3Xir1gf94hF7LsupEC5scJUUDCpQ3eXnFu");
 pub mod counter_2026_05_19 {
     use super::*; 
 
-    // only instruction
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize_counter(ctx: Context<InitializeCounter>) -> Result<()> {
         Ok(()) // returns success
+    }
+
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        // take the current counter value, add 1 safely, then store the result back in the counter
+        // .unwrap() - takes the value out of Some(...)
+        ctx.accounts.counter.count = ctx.accounts.counter.count.checked_add(1).unwrap();
+        Ok(())
     }
 }
 
