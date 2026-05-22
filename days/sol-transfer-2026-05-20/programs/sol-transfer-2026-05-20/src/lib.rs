42ec2741cfa5f673c1b59440c694b639df71a993
0sumTX <0sumtx@gmail.com>
2026-05-21 21:07:13 -0700

    chore: add account definitions

--- days/sol-transfer-2026-05-20/programs/sol-transfer-2026-05-20/src/lib.rs
+++ days/sol-transfer-2026-05-20/programs/sol-transfer-2026-05-20/src/lib.rs
@@ -1,4 +1,6 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{self, Token, TokenAccount, Transfer as SplTransfer};
+use solana_program::system_instruction;
 
 declare_id!("GXhPYbWhpv5BVcivPJJ8LETF8DB5xEgUhPGeQ2UtqVad");
 
@@ -6,11 +8,24 @@ declare_id!("GXhPYbWhpv5BVcivPJJ8LETF8DB5xEgUhPGeQ2UtqVad");
 pub mod sol_transfer_2026_05_20 {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn transfer_lamports(ctx: Context<TransferLamports>, amount: u64) -> Result<()> {
+        
+        // define accounts
+        let from_account = &ctx.accounts.from;
+        let to_account = &ctx.accounts.to;
+
+        // create transfer ix
+        let transfer_instruction = system_instruction::transfer(from_account.key, to_account.key, amount);
+        
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct TransferLamports<'info> {
+    #[account(mut)]
+    pub from: Signer<'info>, // signer of the tx, sends SOL
+    #[account(mut)]
+    pub to: AccountInfo<'info>, // receiver of 'from's SOL
+    pub system_program: Program<'info, System>, // system program to handle transfer
+}
